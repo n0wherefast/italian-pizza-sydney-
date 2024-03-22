af37de59b4975a699557001c0aeea55a219e06c5
n0wherefast <97000096+n0wherefast@users.noreply.github.com>
2024-03-22 17:11:46 +0100

    about section

--- app/pages/about/page.tsx
+++ app/pages/about/page.tsx
@@ -4,33 +4,37 @@ import './about.scss'
 function About() {
   return (
     <div className='mainAbout'>
-      <div className='header'>
-          <div className='image'></div>
+      <div className='sections'>
+        <div className='description'>
+            <h1 className=''>
+            </h1>
+            <p>
+            Mi chiamo Alessandro e vengo dalla Sicilia.
+            Ho cominciato questo mestiere da ragazzino con tanta passione e sacrificio.
+            Adesso sono qui per offrirvi un assaggio di Italia direttamente nelle vostre case.
+        </p>
+        </div>
+        <div className='image'></div>
       </div>
-      <div className='body'>
-          <div className='image'></div>
-      </div>
-      <div className='header'>
-          <div className='image'></div>
-      </div>
-    {/* <div className='description'>
-      <h1 className=' text-2xl'>
-        Salve a tutti
-      </h1>
-      <p>
-      Mi chiamo Alessandro e vengo dalla Sicilia.
-      Ho cominciato questo mestiere da ragazzino con tanta passione e sacrificio.
-      Adesso sono qui per offrirvi un assaggio di Italia direttamente nelle vostre case.
-      </p>
-      <p>
-       Hello everyone! My name is Alessandro and I&apos;m from Sicily. I started this job when I was a kid with a lot of passion and sacrificies. Now I&apos;m here to bring a taste of Italy directly to your place
-      We offer authentic
-      Italian Pizza
-      Mobile wood fire oven to your door for wide range of function and events
+      
 
-      We are here to make your day special such as birthday, family gather, teambonding, wedding etc.
-      </p>
-    </div> */}
+      <div className='mid-sections'>
+        <div className='image'></div>
+        <div className='description'>
+              <p> My name is Alessandro and I&apos;m from Sicily. I started this job when I was a kid with a lot of passion and sacrificies. Now I&apos;m here to bring a taste of Italy directly to your place
+              We offer authentic
+              Italian Pizza
+              Mobile wood fire oven to your door for wide range of function and events
+
+              We are here to make your day special such as birthday, family gather, teambonding, wedding etc.
+              </p>
+        </div>
+      </div>
+
+      {/* <div className='sections'>
+          <div className='image'></div>
+      </div> */}
+    
   </div>
   )
 }
