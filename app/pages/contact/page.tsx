ca262e159db4de2f528769ca03e42b782366276e
n0wherefast <97000096+n0wherefast@users.noreply.github.com>
2024-03-26 19:38:27 +0100

    about section

--- app/pages/contact/page.tsx
+++ app/pages/contact/page.tsx
@@ -7,19 +7,19 @@ function Page() {
     <div className='mainContact'>
     <div className='sectionsContact'>
       <div className='imageContact'></div>
-      <div className='Contact'>
-       
-        <p>Call: 0481.769.450</p>
-        <p>E-mail: Italianpizzasydney88@gmail.com</p>
-   
+      <div className='Social'>
+      
       </div>
       
     </div>
-    
+    <div className='Contact'>
+        <p>Call: 0481.769.450</p>
+        <p>E-mail: Italianpizzasydney88@gmail.com</p>
+      </div>
 
     <div className='mid-sectionsContact'>
       
-      <div className='Contact'>
+      <div className='Social'>
            
       </div>
       <div className='imageContact'></div>
