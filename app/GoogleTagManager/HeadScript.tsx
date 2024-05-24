e95d5d78b3e7f70c66200213ca729b158d53d96e
n0wherefast <97000096+n0wherefast@users.noreply.github.com>
2024-05-24 18:25:16 +0200

    updated GTM

--- app/GoogleTagManager/HeadScript.tsx
+++ app/GoogleTagManager/HeadScript.tsx
@@ -11,10 +11,10 @@ function HeadScript() {
         consent == true ?  <Script id="GTM"
        dangerouslySetInnerHTML={
           {__html:`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5P42Q36H');`}
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5W5QDDKP');`}
         } 
       />   : null
     }
@@ -24,3 +24,5 @@ function HeadScript() {
 }
 
 export default HeadScript
+
+// 
